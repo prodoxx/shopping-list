5f39562b8054ec3dd454f43c4c11b4cb7c82027c
Reggie Gian Escobar <me@reggieescobar.com>
2019-09-29 11:53:25 -0700

    Created route to get items

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dbURI = require('./config/keys').mongoURI;
 
+// routes
+const items = require('./routes/api/items');
+
+// initialize app
 const app = express();
 
 // middlewares
 app.use(bodyParser.json());
 
-const dbURI = require('./config/keys').mongoURI;
-
 // connect to Mongo
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MonogoDB connected...'))
   .catch(err => console.log(err));
+
+// use routes
+app.use('/api/items', items);
 
 // set port
 const port = process.env.PORT || 5000;
